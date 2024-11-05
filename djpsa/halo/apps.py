724cf8caff873c9f07382233cab238ca6379a1e8
Matt Fox <matt@topleft.team>
2024-11-05 14:29:52 -0800

    Makemigrations does any installed app

--- djpsa/halo/apps.py
+++ djpsa/halo/apps.py
@@ -5,6 +5,7 @@ from django.conf import settings
 class HaloConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'djpsa.halo'
+    label = 'halo'
 
     def ready(self):
         from djpsa.halo import provider
